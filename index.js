679a2166676ecebb2b8ae17899e46d3941f63da7
user2695 <zy.khan37@gmail.com>
2022-06-05 19:07:40 +0530

    added route and css folder

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const https = require("https");
-
+const ejs = require("ejs");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/home", (req, res) => {
   const url = "https://api.adviceslip.com/advice";
   https.get(url, (response) => {
     response.on("data", (data) => {
