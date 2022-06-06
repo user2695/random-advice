4d59d25f9cc69a51b7a0285dac39221ba0eae4b2
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-06-06 11:40:15 +0530

    issues solved

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const https = require("https");
-
+const ejs = require("ejs");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   const url = "https://api.adviceslip.com/advice";
   https.get(url, (response) => {
     response.on("data", (data) => {
       const adviceData = JSON.parse(data);
       const advice = adviceData.slip.advice;
-      res.send(advice);
+      res.render("home", { advice });
     });
   });
 });
